ca4ea0aba82094d2b573707680e112b74a5f4fac
KoretsuNobuyasu <einsatzkommandocobra.1996@gmail.com>
2019-02-01 23:35:34 +0900

    タイトルのみ表示

--- top/models.py
+++ top/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+
+
+class Topics(models.Model):
+    """全体に向けてのお知らせ"""
+    title = models.CharField('タイトル', max_length=255)
+    text = models.TextField("本文")
+    created_at = models.DateTimeField('作成日',default=timezone.now)
+
+    def ___str___(self):
+        return self.title
--- top/views.py
+++ top/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect
+from django.views import generic
+from django.urls import reverse_lazy
+from .models import Topics
+ 
+class IndexViews(generic.ListView):
+    model = Topics
 
-# Create your views here.
+    def get_queryset(self):
+        return Topics.objects.order_by('-created_at')
